07cf62a6607d0024810288974ad88f87c491282e
snowsappy <lindatencio12@gmail.com>
2026-08-24 06:57:49 -0500

    cambios nuevos

--- prueba.php
+++ prueba.php
@@ -4,10 +4,24 @@ $empleado_especialidad=[];
 $empleados=[];
 $clientes=[["codigo"=>"","nombre"=>""]];
 $agendamiento_cita=[["codigo_empleado"=>"","codigo_cliente"=>"","especialidad"=>"","hora"=>"","dia"=>""]];
-function agregarempleado(array &$empleados,array $p_empleado_especialidad,string $p_nombre){
+function agregarempleado(int $p_cedula,array &$empleados,array $p_empleado_especialidad,string $p_nombre){
 
-	$empleados[] = ["nombre" => $p_nombre, "especialidad" => $p_empleado_especialidad];
+	$empleados[] = ["cedula"=>$p_cedula,"nombre" => $p_nombre, "especialidad" => $p_empleado_especialidad];
 
+}
+
+function recorrer (array $empleados ,string $p_tipo_especialidad){
+       echo "empleados especializados en esta especialidad";
+       foreach ($empleados as $item){
+			$lista=$item["especialidad"];
+			foreach ($lista as $emple){
+
+				if ($p_tipo_especialidad==$emple){
+					echo " \n nombre: " .$item["nombre"] ."\n";
+					$p=array_column($empleados,"cedula");
+					$posicion=array_search($emple,$empleados);
+			}      }
+	}		
 }
 while (true){
 
@@ -19,20 +33,45 @@ while (true){
 				$especialidad=[];
 				echo "bienvenido al registro de empleado empleado";
 		   		$nombre=readline("ingrese el nombre del empleado a registrar(x para salir): ");
-				$empleados["nombre"]=$especialidad;
-				if($nombre=="x"){
+				
+				if($nombre=="x" ){
 					break;
 				}else{
+					$cedula=readline("ingrese la cedula del empleado a registrar: ");
 					print("aqui va el registro \n ");
 					while (true){
 						
 						echo "digite la especialidad del empleado \n 1.manicurista \n 2.esteticista(limpiadora facial) \n 3.pedicurista \n 4.masajista \n 5. masoterapeuta \n 6. Esteticista corporal \n 7.cosmetóloga \n ";
 						$especialidad=readline();
+						switch($especialidad){
+							case 1:
+								$especialidad="manicurista";
+								break;
+							case 2:
+								$especialidad="esteticista(limpiadora facial)";
+								break;
+							case 3:
+								$especialidad="pedicurista";
+								break;
+							case 4:
+								$especialidad="masajista";
+								break;
+							case 5:
+								$especialidad="masoterapeuta";
+								break;
+							case 6:
+								$especialidad="Esteticista corporal";
+								break;
+							case 7:
+								$especialidad="cosmetóloga";
+								break;
+						}
 						$empleado_especialidad[]=$especialidad;
+						
 						echo "desea agregar otra especialidad";
 						$especialidad=readline();
 						if($especialidad=="no"){
-							agregarempleado($empleados,$empleado_especialidad,$nombre);
+							agregarempleado($cedula,$empleados,$empleado_especialidad,$nombre);
 							
 							$especialidad=[];
 							print_r($empleados);
@@ -45,6 +84,58 @@ while (true){
 			}
 			break;
 		case 2:
+			while (true){
+				echo "REGISTRO DE CITAS";
+				$cliente=readline("digite el nombre de cliente que desea registrar:(x para salir) ");
+				if ($cliente=="x" || trim($cliente) == ''){
+					break;
+				}else{
+					$cedula=readline("digite la cedula del cliente a registrar: ");
+					while (true){
+						$tipo_especialidad=readline("\n especialidades disponibles \n 1. \n 2. \n 3. \n 4. \n 5. \n 6. \n 7. \n ");
+						switch($tipo_especialidad){
+                                                        case 1:
+                                                                $tipo_especialidad="manicurista";
+                                                                recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 2:
+                                                                $tipo_especialidad="esteticista(limpiadora facial)";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 3:
+
+                                                                $tipo_especialidad="pedicurista";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 4:
+                                                                $tipo_especialidad="masajista";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 5:
+                                                                $tipo_especialidad="masoterapeuta";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 6:
+                                                                $tipo_especialidad="Esteticista corporal";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+                                                        case 7:
+                                                                $tipo_especialidad="cosmetóloga";
+								recorrer($empleados,$tipo_especialidad);
+                                                                break;
+						
+                                                }
+						$opcion==readline("desea agendar otro servicio? :")
+						if($opcion=="si"){
+					
+						}else if($opcion=="no"){
+							break;
+						}else{
+						        
+							}
+					}
+				}
+			}
 			break;
 		case 3:
 			break;
@@ -56,4 +147,6 @@ while (true){
 		break;
 		
 	}
+
 }
+
